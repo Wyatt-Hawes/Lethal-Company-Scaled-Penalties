9a4d7881087dbb8bd21fcc95ab7ddb3803e7c4ce
Wyatt H <jhawes@ucsc.edu>
2024-01-01 22:20:05 -0800

    Adding source code

---
+++ source/Patches/HUDManagerPatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+
+namespace ScaledPenalty.Patches
+{
+    [HarmonyPatch(typeof(HUDManager))]
+    internal class HUDManagerPatch
+    {
+        //[HarmonyPatch(nameof(HUDManager.ApplyPenalty))]
+        [HarmonyPatch(typeof(HUDManager), "ApplyPenalty")]
+        [HarmonyPrefix]
+        public static bool ApplyPenalty(ref EndOfGameStatUIElements ___statsUIElements, ref StartOfRound ___playersManager, int playersDead, int bodiesInsured)
+        {
+            //Create penalty based on the players in the lobby
+            ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource("Migis.ScaledPenaltyMod");
+            mls.LogInfo("Inside Game!");
+            mls.LogInfo("Connected Players: " + ___playersManager.connectedPlayersAmount);
+            mls.LogInfo("Connected Players+1: " + (___playersManager.connectedPlayersAmount + 1));
+
+            float penaltyPercentage = (float)(1 / (float)(___playersManager.connectedPlayersAmount + 1.0f));
+
+            mls.LogInfo("Calculated Penalty Per Player: "+ penaltyPercentage);
+
+            Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+            int groupCredits = terminal.groupCredits;
+            bodiesInsured = Mathf.Max(bodiesInsured, 0);
+
+            //Penalty applied when you dont recover a body
+            for (int i = 0; i < playersDead - bodiesInsured; i++)
+            {
+                terminal.groupCredits -= (int)((float)groupCredits * penaltyPercentage);
+            }
+            terminal.groupCredits = Mathf.Max(terminal.groupCredits, 0);
+
+            ___statsUIElements.penaltyAddition.text = $"{playersDead} casualties: -{penaltyPercentage * 100f * (float)(playersDead - bodiesInsured)}%\n({bodiesInsured} friends recovered)";
+            ___statsUIElements.penaltyTotal.text = $"DUE: ${groupCredits - terminal.groupCredits}";
+            Debug.Log($"New group credits after penalty: {terminal.groupCredits}");
+            return false;
+        }
+    }
+}
---
+++ source/plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ScaledPenalty.Patches;
+
+namespace ScaledPenalty
+{
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class ClassPenaltyBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Migis.ScaledPenaltyMod";
+        private const string modName = "Scaled Penalty Mod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ClassPenaltyBase Instance;
+
+        internal ManualLogSource mls;
+
+        //Entry point
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Scaled Penalty Mod Awoken");
+
+            harmony.PatchAll(typeof(ClassPenaltyBase));
+            harmony.PatchAll(typeof(HUDManagerPatch));
+        }
+    }
+}
